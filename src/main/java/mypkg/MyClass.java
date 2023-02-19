3a934d92a7373ce45941ff4406e6d5a8d68e718a
sahilnarwal2111 <500097751@stu.upes.ac.in>
2023-02-19 15:05:37 +0530

    First commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,15 @@
+package mypkg;
+
+public class MyClass {
+	
+	public void show() {
+		System.out.println("Hello DevOps! Welcome to Jenkins");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass obj = new MyClass();
+		obj.show();
+	}
+
+}
